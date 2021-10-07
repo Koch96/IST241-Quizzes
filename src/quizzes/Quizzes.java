c7a715b4e61eeb41d75862a377741ff13e5bdb4e
Stephen Koch <stephen.koch96@gmail.com>
2021-10-07 12:53:58 -0400

    added display for ChoiceQuestion

--- src/quizzes/Quizzes.java
+++ src/quizzes/Quizzes.java
@@ -11,14 +11,26 @@ public class Quizzes {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
-        Question q = new Question();
-        q.setText("Who was the inventor of Java?");
-        q.setAnswer("James Gosling");
+//        Question q = new Question();
+//        q.setText("Who was the inventor of Java?");
+//        q.setAnswer("James Gosling");
         
-        q.display();
+//        q.display();
+//        System.out.print("Your answer: ");
+//        String response = in.nextLine();
+//        System.out.println(q.checkAnswer(response));
+        
+        ChoiceQuestion q2 = new ChoiceQuestion();
+        q2.setText("What country was inventor born in? ");
+        q2.addChoice("Australia", false);
+        q2.addChoice("Canada", true);
+        q2.addChoice("France", false);
+        q2.addChoice("United States", false);
+        
+        q2.display();
         System.out.print("Your answer: ");
         String response = in.nextLine();
-        System.out.println(q.checkAnswer(response));
+        System.out.println(q2.checkAnswer(response));
     }
 
 }
