bb2fe4d071a7f9f8bbfab09b874bcc08c91627f3
Stephen Koch <stephen.koch96@gmail.com>
2021-10-07 12:17:44 -0400

    Initial commit

---
+++ src/quizzes/Question.java
@@ -0,0 +1,13 @@
+package quizzes;
+
+/** @author sak5680 */
+
+public class Question {
+    protected String text;
+    protected String answer;
+    
+    public Question(){
+        text = "";
+        answer = "";
+    }
+}
