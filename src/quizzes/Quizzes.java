fb420b15b0b57adca399a12caf0c01b7f5b97301
Stephen Koch <stephen.koch96@gmail.com>
2021-10-07 12:25:28 -0400

    Starting prompts

--- src/quizzes/Quizzes.java
+++ src/quizzes/Quizzes.java
@@ -10,6 +10,15 @@ public class Quizzes {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
+        Question q = new Question();
+        q.setText("Who was the inventor of Java?");
+        q.setAnswer("James Gosling");
+        
+        q.display();
+        System.out.print("Your answer: ");
+        String response = in.nextLine();
+        System.out.println(q.checkAnswer(response));
     }
 
 }
