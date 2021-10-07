a009fcabeb351663c8fa800e45c5a03985613722
Stephen Koch <stephen.koch96@gmail.com>
2021-10-07 12:45:45 -0400

    Added more classes to ChoiceQuestion

--- src/quizzes/ChoiceQuestion.java
+++ src/quizzes/ChoiceQuestion.java
@@ -5,4 +5,20 @@ import java.util.ArrayList;
 /** @author sak5680 */
 public class ChoiceQuestion extends Question{
     private ArrayList<String> choices;
+    
+    public ChoiceQuestion() {
+        choices = new ArrayList<>();
+    }
+    
+    public void addChoice(String choice, boolean correct){
+        choices.add(choice);
+        if(correct){
+            String choiceString = "" + choices.size();
+            setAnswer(choiceString);
+        }
+    }
+    
+    public void display() {
+        super.display();
+    }
 }
