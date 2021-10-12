fc1e1ae0eb86e53b9aa5b65ebc2e3ddc842ebd07
Stephen Koch <stephen.koch96@gmail.com>
2021-10-12 12:46:04 -0400

    updated with new class.

--- src/quizzes/Quizzes.java
+++ src/quizzes/Quizzes.java
@@ -7,30 +7,28 @@ import java.util.Scanner;
 //@author sak5680
 
 public class Quizzes {
+//    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        Question q = new Question();
+        q.setText("Who was the inventor of Java?");
+        q.setAnswer("James Gosling");
         
-//        Question q = new Question();
-//        q.setText("Who was the inventor of Java?");
-//        q.setAnswer("James Gosling");
-        
-//        q.display();
-//        System.out.print("Your answer: ");
-//        String response = in.nextLine();
-//        System.out.println(q.checkAnswer(response));
-        
-//        ChoiceQuestion q2 = new ChoiceQuestion();
-//        q2.setText("What country was inventor born in? ");
-//        q2.addChoice("Australia", false);
-//        q2.addChoice("Canada", true);
-//        q2.addChoice("France", false);
-//        q2.addChoice("United States", false);
-//        
-//        q2.display();
-//        System.out.print("Your answer: ");
-//        String response = in.nextLine();
-//        System.out.println(q2.checkAnswer(response));
+        ChoiceQuestion q2 = new ChoiceQuestion();
+        q2.setText("What country was inventor born in? ");
+        q2.addChoice("Australia", false);
+        q2.addChoice("Canada", true);
+        q2.addChoice("France", false);
+        q2.addChoice("United States", false);     
+    }
+    
+    public static void presentQuestion(ChoiceQuestion q){
+        q.display();
+        System.out.print("Your answer: ");
+        Scanner sc = new Scanner(System.in);
+        String response = sc.nextLine();
+        System.out.println(q.checkAnswer(response));
     }
 
 }
